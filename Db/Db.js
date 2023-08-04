cc22735516de4d2d0d0e703e05448a0054d57fc8
izroth <lakhansharma889027@gmail.com>
2023-08-04 14:04:44 +0530

    added db

--- Db/Db.js
+++ Db/Db.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
-//connect to database
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
+require('dotenv/config')
+mongoose.connect(
+    process.env.DB_URI,
+    {
+        useNewUrlParser: true,
+       
+    }
+).then(() => {
+    console.log('connected db')
+}).catch((e) => {
+    console.log("An error occured. Error: ",e)
 })
-.then(() => {
-    console.log("Connected to the database!");
-}
-)
-.catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-}
-);
+
+console.log(process.env.DB_URI)
