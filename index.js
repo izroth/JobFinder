81951312a5661c33d120babc9994a33ffa95b62b
izroth <lakhansharma889027@gmail.com>
2023-08-04 13:52:02 +0530

    added nodemon

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = express();
 const port = 3000;
 require('dotenv').config();
 const bodyParser = require('body-parser');
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+
+    }
+);
